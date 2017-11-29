29c289c9bf0ad03bcef5db92d9e3d06f4cdd788a
朱智伟10191772 <zhu.zhiwei@zte.com.cn>
2017-11-29 17:36:03 +0800

    去掉不需要的分号

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
  * 整个应用的入口
  */
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
